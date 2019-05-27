d99540c41590c0542c06c8173020e1960e713992
Daniel Grychtol <grychtol.daniel@gmail.com>
2019-05-27 13:48:53 +0200

    Fix navbar hamburger

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 'use strics';
 
-const btn = document.querySelector('#custom-btn');
+const btn = document.querySelector('#nav-icon3');
 const togglerBtn = document.querySelector('#toggler-btn');
 
 togglerBtn.addEventListener('click', () => {
-  btn.classList.toggle('active');
+  btn.classList.toggle('open');
 });
